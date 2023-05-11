30e0ca9702a246ce9401c614164634a444a53412
marielexpedito <marielespedito@gmail.com>
2023-05-11 16:35:24 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#declaração de variavel estatica/fixa/definida
+#idade = 18
+#vlr_uni = 3.45
+#nome = "Mariel"
+#sobrenome = "Castro"
+
+#declaração de variavel/dinamica
+#nome2 = input("informe seu nome: ")
+#sobrenome2 = input("informe seu sobrenome: ")
+
+#print("nome informado:", nome2)
+#print("sobrenome informado:", sobrenome2)
+
+#só da para concatenar string + string
+# se for int irá realizar a soma dos numeros
+#string + int ocorre erro
+#resultado1 = 8 + "8"
+#print(resultado) -> erro
+
+#Interpolação, utiliza o f na frente do print
+#n1 = int(input('informe o primeiro numero: ')) #conversão de string para int
+#n2 = int("8") #conversão de string para int
+#result = n1 + n2
+
+#oper = input("Informe a operção qiue deseja:  ")
+#print(f'{n1} + {n2} = {result}')
+
+
+#calcular o triangulo, saber se é verdadeiro
+a = 5
+b = 10
+c = 9
+
+#validar se o triangulo é valido
+"""if a >= b + c:
+    # bloco verdadeiro
+    print('a não é menor que b + c')
+    print('não é um triângulo válido')
+elif b >= a + c:
+    print('b não é menor que a + c')
+    print('não é um triângulo válido')
+elif c >= a + b:
+    print('c não é menor que a + b')
+    print('não é um triângulo válido')
+else:
+    # bloco falso
+    print('é um triângulo válido')"""
+
+
+
+
+
